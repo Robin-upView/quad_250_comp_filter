29acfd6dfd22997633f23ef3bd1a782bfa38168b
Robin-upView <robin.gojon@gmail.com>
2015-02-13 18:47:52 +0000

    Autopilot for 250 like quadcopter. Stability is correct. Can’t steer the yaw axis. 15 minutes flight. Quite stable, for vibrations on the roll axis. Maybe gains are too high.

---
+++ quad_250.ino
@@ -0,0 +1,211 @@
+
+#include "Wire.h"
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include "String.h"
+
+#include <Servo.h>
+
+#define CH1  3  // Pin numbers //av gauche
+#define CH2  5  //ar droit
+#define CH3  6  //ar gauche
+#define CH4  7  //av droit
+
+#define ToRad(x) (x*0.01745329252)  // *pi/180
+#define ToDeg(x) (x*57.2957795131)  // *180/pi
+
+MPU6050 accelgyro;
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+volatile unsigned long ulStartPeriod = 0; // set in the interrupt
+volatile boolean bNewThrottleSignal = false; // set in the interrupt and read in the loop
+volatile int rc[7];
+
+float pitch, roll;
+float G_Dt=0.02;
+
+int led = 13;
+
+long timer=0; //general purpose timer 
+long timer_old;
+
+float biasX, biasY, biasZ;
+
+float command_pitch;
+float err_pitch;
+float pid_pitch;
+float pitch_I;
+float pitch_D;
+float err_pitch_old;
+
+float command_roll;
+float err_roll;
+float pid_roll;
+float roll_I;
+float roll_D;
+float err_roll_old;
+
+float throttle;
+
+float pid_yaw;
+float err_yaw;
+float yaw_I;
+
+float kp = 3.0;
+float ki = 0.9; 
+float kd = 0.6;
+
+Servo Servo_1;
+Servo Servo_2;
+Servo Servo_3;
+Servo Servo_4;
+
+void setup()
+{  
+  pinMode(CH1,OUTPUT);
+  pinMode(CH2,OUTPUT);
+  pinMode(CH3,OUTPUT);
+  pinMode(CH4,OUTPUT);
+  
+  Servo_1.attach (CH1, 1000,1900);  //av gauche
+  Servo_2.attach (CH2, 1000,1900);  //ar droit
+  Servo_3.attach (CH3, 1000,1900);  //ar gauche
+  Servo_4.attach (CH4, 1000,1900);  //av droit
+  
+  Servo_1.writeMicroseconds(1000);//arrière droit
+  Servo_2.writeMicroseconds(1000);//avant gauche
+  Servo_3.writeMicroseconds(1000);//arrière gauche
+  Servo_4.writeMicroseconds(1000);
+
+
+  attachInterrupt(0,calcInput,CHANGE);
+  Serial.begin(115200);
+  Wire.begin();
+  
+  pinMode(led, OUTPUT); 
+  accelgyro.setSleepEnabled(false);
+  
+ accelgyro.setFullScaleGyroRange(3); //Gyro scale 2000deg/s
+  delay(1);
+  accelgyro.setFullScaleAccelRange(1);//Accel scale 4g
+  delay(1);
+  accelgyro.setClockSource(3);// Select GyroZ clock
+  delay(1);
+  accelgyro.setDLPFMode(4);// set bandwidth of both gyro and accelerometer to ~20 Hz
+  delay(1);
+  
+
+  calib_gyro();
+
+  timer = micros();
+  delay(20);
+}
+
+
+void loop()
+{
+  if((micros()-timer)>=5000)   
+  { 
+    timer_old = timer;
+    timer=micros();
+    G_Dt = (timer-timer_old)/1000000.0;      // Real time of loop run 
+
+    fast_Loop();
+
+  }
+}
+
+
+void fast_Loop(){
+
+  imu_Valget (); // read sensors
+
+  
+  
+
+  //pid pitch = rc1
+  command_pitch = -(rc[1]-1200.0)/20;
+  err_pitch = command_pitch - pitch;
+  pitch_D = (float)(-gx+biasX)*2000.0f/32768.0f;
+  pitch_I += (float)err_pitch*G_Dt; 
+  //pitch_I = constrain(pitch_I,-50,50);
+  pid_pitch = err_pitch*kp+pitch_I*ki+pitch_D*kd; //P=10 I=15 D=5 was good //D=8 the limit //P=15 I=30 D=5
+  //pid_pitch = 0;
+
+  //ROLL
+  command_roll = (rc[0]-1200.0)/20;
+  err_roll = command_roll - roll;
+  roll_D = (float)(gy-biasY)*2000.0f/32768.0f;
+  roll_I += (float)err_roll*G_Dt; 
+  //roll_I = constrain(roll_I,-50,50);
+  pid_roll = err_roll*kp+roll_I*ki+roll_D*kd; //P=0.1 I=0.85 D=0.1
+  //pid_roll=0; //à supprimer
+
+  //YAW
+  err_yaw = (float)(-(gz-biasZ))*2000.0f/32768.0f;
+  yaw_I += (float)err_yaw*G_Dt; 
+  pid_yaw = err_yaw*6.0+yaw_I*6.0;
+  //pid_yaw=0;
+  //Throttle
+
+  throttle = constrain(rc[2]*1.20,1000,1900);
+
+  if(throttle < 1100)
+  {
+    pid_pitch=0;
+    pid_roll=0;
+    pid_yaw=0;
+
+    pitch_I=0;
+    roll_I=0;
+    yaw_I=0;
+  }
+
+  IMU_print();
+
+  Servo_2.writeMicroseconds(constrain(throttle+pid_roll-pid_pitch+pid_yaw,1000,1900));//arrière droit
+  Servo_1.writeMicroseconds(constrain(throttle-pid_roll+pid_pitch+pid_yaw,1000,1900));//avant gauche
+  Servo_3.writeMicroseconds(constrain(throttle-pid_roll-pid_pitch-pid_yaw,1000,1900));//arrière gauche
+  Servo_4.writeMicroseconds(constrain(throttle+pid_roll+pid_pitch-pid_yaw,1000,1900));//avant droit
+
+
+/*
+  Servo_1.writeMicroseconds(throttle); //set servo position 
+  Servo_2.writeMicroseconds(throttle);
+  Servo_3.writeMicroseconds(throttle); 
+  Servo_4.writeMicroseconds(throttle);
+*/
+}
+
+void calcInput()
+{
+  static unsigned int nThrottleIn;
+  static int channel;
+
+  if(digitalRead(2) == HIGH)
+  { 
+    ulStartPeriod = micros();
+  }
+  else
+  {
+    if(ulStartPeriod)
+    {
+      nThrottleIn = (int)(micros() - ulStartPeriod);
+      ulStartPeriod = 0;
+
+      if(nThrottleIn >2000){
+        channel = 0;
+      }
+      else
+      {
+        rc[channel]=nThrottleIn;
+        channel++;
+      }
+    }
+  }
+}
+
+
+
